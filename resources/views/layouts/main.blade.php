9c169973cd55a6fcbd897fdfeb094c555ea9d2ae
MarcoCalabretta1988 <calabretta.mar@gmail.com>
2023-03-06 15:52:02 +0100

    Add font awsome

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -7,6 +7,9 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="shortcut icon" href="icon-dev.webp" type="image/x-icon">
     <title>{{ env('APP_NAME') }} | @yield('title') </title>
+    {{-- ADD FONTAWSOME --}}
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.css' integrity='sha512-FA9cIbtlP61W0PRtX36P6CGRy0vZs0C2Uw26Q1cMmj3xwhftftymr0sj8/YeezDnRwL9wtWw8ZwtCiTDXlXGjQ==' crossorigin='anonymous'/>
+
     @yield('cdns')
     @vite('resources/js/app.js')
 </head>
