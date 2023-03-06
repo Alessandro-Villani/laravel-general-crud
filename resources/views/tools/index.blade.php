f73f0f44148470df0f74c40d86ce15fe3deb7108
Alessandro-Villani <alessvill@libero.it>
2023-03-06 15:54:47 +0100

    added base card structure

--- resources/views/tools/index.blade.php
+++ resources/views/tools/index.blade.php
@@ -3,16 +3,28 @@
 @section('title', 'Tools')
 
 @section('content')
-    
-    <div class="container py-5">
-        <div class="row row-cols-3">
-            @foreach ($tools as $tool)
-                <div class="col mb-3">
-                    <div class="card d-flex">
-                        <img src="" alt="">
+    <main id="tools">
+        <div class="container py-5">
+            <div class="row row-cols-3">
+                @foreach ($tools as $tool)
+                    <div class="col mb-3">
+                        <a href='#' class="tool-card d-flex p-2">
+                            <div class="col-3 me-3">
+                                <img class="img-fluid" src="{{$tool->img_url}}" alt="{{$tool->name}}">
+                            </div>
+                                <div class="col-9 tool-data">
+                                    <h4 class="mb-1">{{$tool->name}}</h4>
+                                    <p class="category mb-0">{{ $tool->category }}</p>
+                                    <div class="rating">
+                                        @for ($i = 1; $i <= $count; $i++)
+                                            <i class="fa-solid fa-star"></i>
+                                        @endfor
+                                    </div>
+                                </div>
+                            </a>
                     </div>
-                </div>
-            @endforeach
+                @endforeach
+            </div>
         </div>
-    </div>
+    </main>
 @endsection
