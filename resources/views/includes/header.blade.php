299e79457e83035a6043991ac50576d7f8a20b57
Alessandro-Villani <alessvill@libero.it>
2023-03-06 17:38:08 +0100

    edited active class placement in header

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -7,10 +7,10 @@
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="{{ url('/')}}">Home</a>
+            <a class="nav-link {{ request()->routeIs('home') ? 'active' : '' }}" aria-current="page" href="{{ url('/')}}">Home</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="{{ route('tools.index')}}">Tools</a>
+            <a class="nav-link {{ request()->routeIs("tools*") ? 'active' : '' }}" href="{{ route('tools.index')}}">Tools</a>
           </li>
         </ul>
       </div>
